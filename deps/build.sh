70f8ab61a1e3dd9e1b4ec1d61e5c58369791c206
peter <xianrenb@gmail.com>
2014-09-20 22:47:18 +0800

    changed build.sh

--- deps/build.sh
+++ deps/build.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 touch deps.jl
 rm deps.jl
+PKG_DIR=`julia -e 'println(Pkg.dir());'`
 
 cat > deps.jl <<EOT
 macro checked_lib(libname, path)
         (dlopen_e(path) == C_NULL) && error("Unable to load \n\n\$libname (\$path)\n\nPlease re-run Pkg.build(package), and restart Julia.")
         quote const \$(esc(libname)) = \$path end
     end
-@checked_lib mylib "${HOME}/.julia/v0.3/MyLib/deps/usr/lib/libmylib.so.1"
+@checked_lib mylib "${PKG_DIR}/MyLib/deps/usr/lib/libmylib.so.1"
 EOT
 
 cd src/mylib/
