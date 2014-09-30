3d8f2730f36fcbe951842c04492fe7a8d867e968
peter <xianrenb@gmail.com>
2014-09-30 19:50:27 +0800

    changed to use Julia instead of sh script

--- deps/build.jl
+++ deps/build.jl
@@ -1,1 +1,14 @@
-run(`./build.sh`)
+run(`touch deps.jl`)
+run(`rm deps.jl`)
+
+run(`echo 'macro checked_lib(libname, path)
+        (dlopen_e(path) == C_NULL) && error("Unable to load \n\n$libname ($path)\n\nPlease re-run Pkg.build(package), and restart Julia.")
+        quote const $(esc(libname)) = $path end
+    end
+@checked_lib mylib "'"$(Pkg.dir())"'/MyLib/deps/usr/lib/libmylib.so.1"
+'` |> "deps.jl")
+
+cd("src/mylib/")
+run(`make clean`)
+run(`make all`)
+cd("../../")
